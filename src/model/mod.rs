0376818ae4e0ceece80ea782555031ba3077de3e
jamesbt365 <jamesbt365@gmail.com>
2025-06-29 20:53:51 +0100

    ComponentsV2 support (#3123)

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -62,7 +62,6 @@ pub use self::timestamp::Timestamp;
 /// use serenity::model::prelude::*;
 /// ```
 pub mod prelude {
-    pub(crate) use serde::de::Visitor;
     pub(crate) use serde::{Deserialize, Deserializer};
 
     pub use super::guild::automod::EventType as AutomodEventType;
