457f1a717c9c82c4de09e9c63a05c9debe009aad
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2025-10-06 14:47:11 -0400

    Add support for incident actions (#3424)
    Rebase of #3079 now that this feature is stable and documented.
    
    ---------
    
    Co-authored-by: jamesbt365 <jamesbt365@gmail.com>

--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -1,5 +1,6 @@
 use serde::Serialize;
 
+use super::IncidentsData;
 #[cfg(feature = "model")]
 use crate::builder::{
     CreateChannel,
@@ -176,6 +177,10 @@ pub struct PartialGuild {
     pub stickers: HashMap<StickerId, Sticker>,
     /// Whether the guild has the boost progress bar enabled
     pub premium_progress_bar_enabled: bool,
+    /// The id of the channel where this guild will recieve safety alerts.
+    pub safety_alerts_channel_id: Option<ChannelId>,
+    /// The incidents data for this guild, if any.
+    pub incidents_data: Option<IncidentsData>,
 }
 
 #[cfg(feature = "model")]
@@ -1708,6 +1713,8 @@ impl From<Guild> for PartialGuild {
             preferred_locale: guild.preferred_locale,
             max_stage_video_channel_users: guild.max_stage_video_channel_users,
             premium_progress_bar_enabled: guild.premium_progress_bar_enabled,
+            safety_alerts_channel_id: guild.safety_alerts_channel_id,
+            incidents_data: guild.incidents_data,
         }
     }
 }
